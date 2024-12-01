e0804b8820a4cd5db485dba96b8a472676330a2a
mac <hendriarga28@gmail.com>
2024-12-01 07:08:15 +0700

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'agni-guide',
+  webDir: 'www'
+};
+
+export default config;
