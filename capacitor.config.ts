58812a1291e2c9482cea46230f7364934773c3f4
mac <hendriarga28@gmail.com>
2024-12-08 09:02:00 +0700

    Home

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,9 +1,14 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'agni-guide',
-  webDir: 'www'
+  appId: 'com.agniguide.app',
+  appName: 'AgniGuide',
+  webDir: 'www',
+  plugins: {
+    CapacitorHttp: {
+      enabled: true,
+    },
+  }
 };
 
 export default config;
--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,18 +1,41 @@
-import { AfterViewInit, Component } from '@angular/core';
-import { IonContent, IonInput, IonSearchbar } from '@ionic/angular/standalone';
+import { AfterViewInit, Component, inject, signal } from '@angular/core';
+import { IonContent, IonSearchbar, IonToggle, IonGrid, IonRow, IonCol, IonIcon, IonFab, IonFabButton } from '@ionic/angular/standalone';
+import { addIcons } from 'ionicons';
+import { gridOutline, add } from 'ionicons/icons';
 import mapboxgl from 'mapbox-gl';
-import { environment } from 'src/environments/environment.prod';
+import { environment } from 'src/environments/environment.local';
+import { NavController, ActionSheetController } from '@ionic/angular';
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss'],
   standalone: true,
-  imports: [IonSearchbar, IonInput, IonContent],
+  imports: [
+    IonGrid, 
+    IonRow, 
+    IonCol, 
+    IonToggle, 
+    IonIcon, 
+    IonSearchbar, 
+    IonContent,
+    IonFab,
+    IonFabButton,
+  ],
 })
 export class Tab1Page implements AfterViewInit {
+  protected nav = inject(NavController);
+  actionSheetCtrl = inject(ActionSheetController);
+
   map: null|mapboxgl.Map = null;
+  mapLng = signal<number>(environment.mapbox.defaultLng)
+  mapLat = signal<number>(environment.mapbox.defaultLat)
+  mapZoom = signal<number>(10)
+  gridMode = signal<boolean>(false)
+
   constructor() {
+    addIcons({gridOutline,add});
+
     mapboxgl.accessToken = environment.mapbox.accessToken;
   }
 
@@ -24,8 +47,33 @@ export class Tab1Page implements AfterViewInit {
     this.map = new mapboxgl.Map({
       container: 'mapbox-container',
       style: 'mapbox://styles/mapbox/streets-v11',
-      center: [-74.5, 40],
-      zoom: 9
+      center: [this.mapLng(), this.mapLat()],
+      zoom: this.mapZoom()
     });
+  }
+
+  async toggleGridMode(ev: any) {
+    this.gridMode.update((value) => value = ev.detail.checked);
+
+    if (this.gridMode()) {
+      const actionSheet = await this.actionSheetCtrl.create({
+        header: 'Aktifkan Grid Mode',
+        mode: 'md',
+        cssClass: 'action-sheet',
+        buttons: [
+          {
+            text: 'Lanjutkan',
+            handler: () => this.nav.navigateRoot('/historic-site')
+          },
+          {
+            text: 'Batalkan',
+            role: 'cancel',
+            handler: () => this.gridMode.update((value) => value = false)
+          }
+        ]
+      });
+
+      actionSheet.present();
+    }
   }
 }
