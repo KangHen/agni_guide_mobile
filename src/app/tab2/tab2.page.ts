4b487bf4f754d09bb6eea4bd856e734c3a6630d0
mac <hendriarga28@gmail.com>
2024-12-12 20:57:03 +0700

    News

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,18 +1,19 @@
 import { Component, inject, OnInit, signal } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent, IonGrid, IonCol, IonRow } from '@ionic/angular/standalone';
+import { IonHeader, IonToolbar, IonTitle, IonContent, IonGrid, IonCol, IonRow, IonInfiniteScroll, IonInfiniteScrollContent } from '@ionic/angular/standalone';
 import { News } from '../pages/read-news/news.type';
 import { NewsCardComponent } from '../components/news-card/news-card.component';
 import { NotFoundComponent } from '../shared/not-found/not-found.component';
 import { NavController } from '@ionic/angular';
 import { NewsService } from '../pages/read-news/news.service';
 import { HelperService } from '../services/helper.service';
+import { InfiniteScrollCustomEvent } from '@ionic/angular';
 
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
   styleUrls: ['tab2.page.scss'],
   standalone: true,
-  imports: [
+  imports: [IonInfiniteScrollContent, IonInfiniteScroll, 
     IonRow, 
     IonCol, 
     IonGrid,
@@ -29,7 +30,9 @@ export class Tab2Page implements OnInit {
   helperService = inject(HelperService);
   newsService = inject(NewsService);
 
+  page = signal<number>(1);
   items = signal<News[]>([]);
+  isComplete = signal<boolean>(false);
 
   constructor() {}
 
@@ -50,6 +53,21 @@ export class Tab2Page implements OnInit {
       loading.dismiss();
       this.helperService.presentError(error?.message);
     }
+  }
+
+ async onIonInfinite(ev: any): Promise<void> {
+    this.page.update(value => value=+1);
+
+    try {
+      const { data } = await this.newsService.all({ page: this.page() });
+      this.items.update(value => [...value, ...data]);
+    } catch (error: any) {
+      this.helperService.presentError(error?.message);
+    }
+
+    setTimeout(() => {
+      (ev as InfiniteScrollCustomEvent).target.complete();
+    }, 500);
   }
 
   readNews(slug: string) {
